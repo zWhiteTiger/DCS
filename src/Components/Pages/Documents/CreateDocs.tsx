0745d426cdf1371b45ff10e0cc4b96b774630905
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2024-10-09 19:12:01 +0700

    2024

--- src/Components/Pages/Documents/CreateDocs.tsx
+++ src/Components/Pages/Documents/CreateDocs.tsx
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
-import { Grid, Card, CardContent, Typography, Box, styled } from '@mui/material';
-import { Steps, Button, message } from 'antd';
-import NoMoreContent from '../Utility/NoMoreContent';
-import PDFServices from './Services/PDFServices';
-import Uploader from './Services/Uploader';
+import React, { useRef, useState } from 'react'
+import { Grid, Card, CardContent, Typography, Box, styled } from '@mui/material'
+import { Steps, Button, message } from 'antd'
+import NoMoreContent from '../Utility/NoMoreContent'
+import PDFServices from './Services/PDFServices'
+import Uploader from './Services/Uploader'
 
 const CreateDocs: React.FC = () => {
-  const [currentStep, setCurrentStep] = useState(0);
-  const [fileUrl, setFileUrl] = useState<string>('');
-
-  const StyledCard = styled(Card)(({ }) => ({
+  const [currentStep, setCurrentStep] = useState(0)
+  const [fileUrl, setFileUrl] = useState<string>('')
+  // const [docId, setDocId] = useState<string | null>(null)
+  const docIdRef = useRef<string | null>(null)
+  const StyledCard = styled(Card)(({}) => ({
     borderRadius: '10px',
     boxShadow: '0px 0px 10px rgba(255, 255, 255, 0)', // Drop shadow with color #FFF
-  }));
+  }))
 
   const nextStep = () => {
-    setCurrentStep(currentStep + 1);
-  };
+    setCurrentStep(currentStep + 1)
+  }
 
   const prevStep = () => {
-    setCurrentStep(currentStep - 1);
-  };
+    setCurrentStep(currentStep - 1)
+  }
 
   const handleDone = () => {
     message.success({
       content: 'ดำเนินการเอกสารเสร็จสิ้น',
       className: 'ant-message-custom-style',
-    });
-  };
+    })
+  }
+
+  // console.log(docIdRef.current)
+  // 670669b81732907abf54c64b
 
   const steps = [
     {
@@ -35,17 +39,20 @@ const CreateDocs: React.FC = () => {
       content: (
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Typography variant="h4">ขั้นตอนที่ 1 อัพโหลดไฟล์</Typography>
-            <Typography>รองรับไฟล์สกุล .PDF เท่านั้น และ ไฟล์จะต้องมีขนาดไม่เกิน 50 MB.</Typography>
+            <Typography variant='h4'>ขั้นตอนที่ 1 อัพโหลดไฟล์</Typography>
+            <Typography>
+              รองรับไฟล์สกุล .PDF เท่านั้น และ ไฟล์จะต้องมีขนาดไม่เกิน 50 MB.
+            </Typography>
           </Grid>
           <Grid item xs={12}>
-            <Box className="p-9">
+            <Box className='p-9'>
               <Uploader
                 nextStep={nextStep}
                 setFileUrl={(url: string) => {
-                  setFileUrl(url);
-                  setCurrentStep(1); // Automatically move to Step 2
+                  setFileUrl(url)
+                  setCurrentStep(1) // Automatically move to Step 2
                 }}
+                setDocId={docIdRef}
               />
             </Box>
           </Grid>
@@ -56,14 +63,23 @@ const CreateDocs: React.FC = () => {
       title: <Typography>แก้ไข</Typography>,
       content: (
         <Box>
-          <Typography variant="h4">ขั้นตอนที่ 2 แก้ไขเอกสาร</Typography>
+          <Typography variant='h4'>ขั้นตอนที่ 2 แก้ไขเอกสาร</Typography>
           <Typography>แก้ไขเอกสารหรือเพิ่มผู้ลงนาม</Typography>
           <StyledCard>
-            {fileUrl ? <PDFServices fileUrl={fileUrl} /> :
-              <Box className='flex justify-center' style={{ backgroundColor: '#ec4649', color: '#FFFFFF', height: '100px' }}>
+            {fileUrl ? (
+              <PDFServices fileUrl={fileUrl} docId={docIdRef.current} />
+            ) : (
+              <Box
+                className='flex justify-center'
+                style={{
+                  backgroundColor: '#ec4649',
+                  color: '#FFFFFF',
+                  height: '100px',
+                }}
+              >
                 <Typography className='mt-9'>กรุณาอัพโหลดไฟล์ก่อน</Typography>
               </Box>
-            }
+            )}
           </StyledCard>
         </Box>
       ),
@@ -73,20 +89,28 @@ const CreateDocs: React.FC = () => {
       content: (
         <Grid container spacing={2}>
           <Grid item xs={9}>
-            <Typography variant="h4">ขั้นตอนที่ 3 ดำเนินการส่งเอกสาร</Typography>
+            <Typography variant='h4'>
+              ขั้นตอนที่ 3 ดำเนินการส่งเอกสาร
+            </Typography>
             <Typography>ตรวจสอบความเรียบร้อยก่อนส่งเอกสาร</Typography>
           </Grid>
           <Grid item xs={3}>
-            <Typography variant="h5">รายชื่อผู้ลงนาม</Typography>
+            <Typography variant='h5'>รายชื่อผู้ลงนาม</Typography>
           </Grid>
         </Grid>
       ),
     },
-  ];
+  ]
 
   return (
     <>
-      <Card style={{ borderRadius: '10px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0)', padding: '25px' }}>
+      <Card
+        style={{
+          borderRadius: '10px',
+          boxShadow: '0px 0px 10px rgba(0, 0, 0, 0)',
+          padding: '25px',
+        }}
+      >
         <CardContent>
           <Steps current={currentStep}>
             {steps.map((step, index) => (
@@ -98,17 +122,44 @@ const CreateDocs: React.FC = () => {
             {currentStep > 0 && (
               <>
                 {currentStep > 1 && (
-                  <Button size='large' style={{ marginRight: '10px', color: 'white', backgroundColor: '#4318FF', fontFamily: 'Kanit' }} onClick={prevStep}>
+                  <Button
+                    size='large'
+                    style={{
+                      marginRight: '10px',
+                      color: 'white',
+                      backgroundColor: '#4318FF',
+                      fontFamily: 'Kanit',
+                    }}
+                    onClick={prevStep}
+                  >
                     ก่อนหน้า
                   </Button>
                 )}
                 {currentStep < steps.length - 1 && (
-                  <Button size='large' type="primary" onClick={nextStep} style={{ color: 'white', backgroundColor: '#4318FF', fontFamily: 'Kanit' }}>
+                  <Button
+                    size='large'
+                    type='primary'
+                    onClick={nextStep}
+                    style={{
+                      color: 'white',
+                      backgroundColor: '#4318FF',
+                      fontFamily: 'Kanit',
+                    }}
+                  >
                     ถัดไป
                   </Button>
                 )}
                 {currentStep === steps.length - 1 && (
-                  <Button size='large' type="primary" onClick={handleDone} style={{ color: 'white', backgroundColor: '#4318FF', fontFamily: 'Kanit' }}>
+                  <Button
+                    size='large'
+                    type='primary'
+                    onClick={handleDone}
+                    style={{
+                      color: 'white',
+                      backgroundColor: '#4318FF',
+                      fontFamily: 'Kanit',
+                    }}
+                  >
                     เสร็จสิ้น
                   </Button>
                 )}
@@ -119,7 +170,7 @@ const CreateDocs: React.FC = () => {
       </Card>
       <NoMoreContent />
     </>
-  );
-};
+  )
+}
 
-export default CreateDocs;
+export default CreateDocs
--- src/Components/Pages/Documents/Services/Uploader.tsx
+++ src/Components/Pages/Documents/Services/Uploader.tsx
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
-import { Upload, message, Typography, Spin } from 'antd';
-import { UploadProps } from 'antd/es/upload';
-import Box from '@mui/material/Box';
-import { LuHardDriveUpload } from 'react-icons/lu';
-import { useAppDispatch } from '../../../../Store/Store';
-import { setPath } from '../../../../Store/Slices/pathSlice';
-const { Dragger } = Upload;
+import React, { useState } from 'react'
+import { Upload, message, Typography, Spin } from 'antd'
+import { UploadProps } from 'antd/es/upload'
+import Box from '@mui/material/Box'
+import { LuHardDriveUpload } from 'react-icons/lu'
+import { useAppDispatch } from '../../../../Store/Store'
+import { setPath } from '../../../../Store/Slices/pathSlice'
+const { Dragger } = Upload
 
 type UploaderProps = {
-  setFileUrl: (url: string) => void;
-  nextStep: () => void;
-};
+  setFileUrl: (url: string) => void
+  nextStep: () => void
+  setDocId: React.MutableRefObject<string | null>
+}
 
-const Uploader: React.FC<UploaderProps> = ({ setFileUrl, nextStep }) => {
-  const [loading, setLoading] = useState(false);  // เพิ่ม state สำหรับการควบคุมสถานะการอัพโหลด
-  const disPatch = useAppDispatch();
+const Uploader: React.FC<UploaderProps> = ({
+  setFileUrl,
+  nextStep,
+  setDocId,
+}) => {
+  const [loading, setLoading] = useState(false) // เพิ่ม state สำหรับการควบคุมสถานะการอัพโหลด
+  const disPatch = useAppDispatch()
 
   const props: UploadProps = {
     name: 'file',
@@ -26,51 +31,59 @@ const Uploader: React.FC<UploaderProps> = ({ setFileUrl, nextStep }) => {
       doc_name: file.name,
     }),
     beforeUpload(file) {
-      const isPdf = file.type === 'application/pdf';
+      const isPdf = file.type === 'application/pdf'
       if (!isPdf) {
-        message.error('ไฟล์ต้องเป็น .PDF เท่านั้น!');
-        return Upload.LIST_IGNORE;
+        message.error('ไฟล์ต้องเป็น .PDF เท่านั้น!')
+        return Upload.LIST_IGNORE
       }
-      const isLt50M = file.size / 1024 / 1024 < 50;
+      const isLt50M = file.size / 1024 / 1024 < 50
       if (!isLt50M) {
-        message.error('ขนาดไฟล์ต้องน้อยกว่า 50MB');
-        return Upload.LIST_IGNORE;
+        message.error('ขนาดไฟล์ต้องน้อยกว่า 50MB')
+        return Upload.LIST_IGNORE
       }
-      setLoading(true); // เริ่มการแสดงผลสถานะการอัพโหลด
-      return true;
+      setLoading(true) // เริ่มการแสดงผลสถานะการอัพโหลด
+      return true
     },
     onChange(info) {
-      const { status, response } = info.file;
+      const { status, response } = info.file
       if (status === 'done') {
-        message.success(`อัพโหลด ${info.file.name} เสร็จสิ้น`);
-        setLoading(false);  // อัพโหลดเสร็จสิ้น ปิดสถานะการโหลด
-        if (response?.url) {
-          setFileUrl(response.url);
-          disPatch(setPath(response.url));
-          nextStep();  // ไปยังสเต็ปถัดไป
+        message.success(`อัพโหลด ${info.file.name} เสร็จสิ้น`)
+        setLoading(false) // อัพโหลดเสร็จสิ้น ปิดสถานะการโหลด
+        if (response.url) {
+          setFileUrl(response.url)
+          disPatch(setPath(response.url))
+          setDocId.current = response.docID
+          // setDocId(response.docID)
+          nextStep() // ไปยังสเต็ปถัดไป
         }
       } else if (status === 'error') {
-        message.error(`อัพโหลดไฟล์ ${info.file.name} ล้มเหลว`);
-        setLoading(false);  // หากอัพโหลดล้มเหลว ปิดสถานะการโหลด
+        message.error(`อัพโหลดไฟล์ ${info.file.name} ล้มเหลว`)
+        setLoading(false) // หากอัพโหลดล้มเหลว ปิดสถานะการโหลด
       }
     },
-  };
+  }
 
   return (
-    <Spin spinning={loading} tip="กำลังอัพโหลด...">  {/* เพิ่ม Spin component เพื่อแสดงการโหลด */}
+    <Spin spinning={loading} tip='กำลังอัพโหลด...'>
+      {' '}
+      {/* เพิ่ม Spin component เพื่อแสดงการโหลด */}
       <Dragger {...props}>
-        <Box className="flex justify-center my-5">
+        <Box className='flex justify-center my-5'>
           <LuHardDriveUpload style={{ fontSize: '50px', color: 'blue' }} />
         </Box>
-        <Typography style={{ fontSize: '18px', color: '#000', fontFamily: 'Kanit' }}>
+        <Typography
+          style={{ fontSize: '18px', color: '#000', fontFamily: 'Kanit' }}
+        >
           คลิ๊กหรือลากวางไฟล์ลงตรงนี้
         </Typography>
-        <Typography style={{ fontSize: '12px', color: '#5d5d5d', fontFamily: 'Kanit' }}>
+        <Typography
+          style={{ fontSize: '12px', color: '#5d5d5d', fontFamily: 'Kanit' }}
+        >
           รองรับไฟล์สกุล .PDF เท่านั้น ขนาดไม่เกิน 50 MB.
         </Typography>
       </Dragger>
     </Spin>
-  );
-};
+  )
+}
 
-export default Uploader;
+export default Uploader
